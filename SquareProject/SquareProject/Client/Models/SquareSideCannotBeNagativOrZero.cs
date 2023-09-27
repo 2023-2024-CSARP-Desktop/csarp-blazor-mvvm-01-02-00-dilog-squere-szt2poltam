7fe74b78c81d4db3e08c0c86d6f804ac283b1a9b
Tamás Polyakovich <polyakovichtamas@gmail.com>
2023-09-27 10:21:27 +0200

    Mappák a kódokkal létrehozva

---
+++ SquareProject/SquareProject/Client/Models/SquareSideCannotBeNagativOrZero.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.Serialization;
+
+
+
+
+namespace SquareProject.Models
+
+{
+
+    [Serializable]
+
+    internal class SquareSideCannotBeNegativeOrZero : Exception
+
+    {
+
+        public SquareSideCannotBeNegativeOrZero()
+
+        {
+
+        }
+
+
+
+
+        public SquareSideCannotBeNegativeOrZero(string? message) : base(message)
+
+        {
+
+        }
+
+
+
+
+        public SquareSideCannotBeNegativeOrZero(string? message, Exception? innerException) : base(message, innerException)
+
+        {
+
+        }
+
+
+
+
+        protected SquareSideCannotBeNegativeOrZero(SerializationInfo info, StreamingContext context) : base(info, context)
+
+        {
+
+        }
+
+    }
+
+}
